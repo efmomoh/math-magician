1991746672070f57f01a54ddfdc14c8eb0f4e696
efmomoh <efmomoh25@gmail.com>
2023-09-22 08:41:34 +0000

    Update the calculator dot js file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,37 +1,70 @@
 import './Calculator.css';
+import CalculatorButton from './CalculatorButton';
 
 function Calculator() {
   return (
     <div className="calculator-wrapper">
       <div className="row1 Display">0</div>
       <div className="row2">
-        <button type="button" className="calculator-button">AC</button>
-        <button type="button" className="calculator-button">+/-</button>
-        <button type="button" className="calculator-button">%</button>
-        <button type="button" className="calculator-button calculator-operator">&#247;</button>
+        <CalculatorButton className="calculator-button">AC</CalculatorButton>
+        <CalculatorButton className="calculator-button">+/-</CalculatorButton>
+        <CalculatorButton className="calculator-button">%</CalculatorButton>
+        <CalculatorButton className="calculator-button calculator-operator">
+          &#247;
+        </CalculatorButton>
       </div>
       <div className="row3">
-        <button type="button" className="calculator-button">7</button>
-        <button type="button" className="calculator-button">8</button>
-        <button type="button" className="calculator-button">9</button>
-        <button type="button" className="calculator-button calculator-operator">&#215;</button>
+        <button type="button" className="calculator-button">
+          7
+        </button>
+        <button type="button" className="calculator-button">
+          8
+        </button>
+        <button type="button" className="calculator-button">
+          9
+        </button>
+        <button type="button" className="calculator-button calculator-operator">
+          &#215;
+        </button>
       </div>
       <div className="row4">
-        <button type="button" className="calculator-button">4</button>
-        <button type="button" className="calculator-button">5</button>
-        <button type="button" className="calculator-button">6</button>
-        <button type="button" className="calculator-button calculator-operator">-</button>
+        <button type="button" className="calculator-button">
+          4
+        </button>
+        <button type="button" className="calculator-button">
+          5
+        </button>
+        <button type="button" className="calculator-button">
+          6
+        </button>
+        <button type="button" className="calculator-button calculator-operator">
+          -
+        </button>
       </div>
       <div className="row5">
-        <button type="button" className="calculator-button">1</button>
-        <button type="button" className="calculator-button">2</button>
-        <button type="button" className="calculator-button">3</button>
-        <button type="button" className="calculator-button calculator-operator">+</button>
+        <button type="button" className="calculator-button">
+          1
+        </button>
+        <button type="button" className="calculator-button">
+          2
+        </button>
+        <button type="button" className="calculator-button">
+          3
+        </button>
+        <button type="button" className="calculator-button calculator-operator">
+          +
+        </button>
       </div>
       <div className="row6">
-        <button type="button" className="calculator-button-zero">0</button>
-        <button type="button" className="calculator-button">.</button>
-        <button type="button" className="calculator-button calculator-operator">=</button>
+        <button type="button" className="calculator-button-zero">
+          0
+        </button>
+        <button type="button" className="calculator-button">
+          .
+        </button>
+        <button type="button" className="calculator-button calculator-operator">
+          =
+        </button>
       </div>
     </div>
   );
