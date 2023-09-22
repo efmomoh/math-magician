5f24fc06c2da49fe11461364e7ab96f62d785771
efmomoh <efmomoh25@gmail.com>
2023-09-22 12:38:14 +0000

    Updates App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import './App.css';
 import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div>
+      <h1>Math Magician</h1>
       <Calculator />
-
     </div>
   );
 }
