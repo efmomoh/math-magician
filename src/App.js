4d187ff9a995884c6d50f6ce61e83bf30af9c331
efmomoh <efmomoh25@gmail.com>
2023-09-28 18:04:22 +0000

    Update the App.js file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
-import ShowQuote from './components/Quote';
+import Quote from './components/Quote';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <section>
-      <h1>Math Magician</h1>
-      <main className="contents">
-        <ShowQuote />
-        <Calculator />
-      </main>
-    </section>
+    <>
+      <nav className="nav">
+        <h1 className="heading">Math Magicians</h1>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li className="pipe">|</li>
+          <span />
+          <li>
+            <NavLink to="/calculator">Calculator</NavLink>
+          </li>
+          <li className="pipe">|</li>
+
+          <span />
+          <li>
+            <NavLink to="/quote">Quote</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
